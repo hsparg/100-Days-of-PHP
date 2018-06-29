05113d6cb13ed6779994af378b35bccfe9c2691d
Haley Sparger <hsparg@users.noreply.github.com>
2018-06-29 14:38:23 -0500

    Create Switch_Statement_using_numbers.php

---
+++ Switch_Statement_using_numbers.php
@@ -0,0 +1,16 @@
+<?php
+//Example of a Switch Statement
+
+$total = 55.99; 
+$delivery = 10; 
+
+switch($total){
+	case $total < 50:
+		echo 'Total (including delivery): ' . $total += $delivery;
+		break; 
+	case $total < 100:
+		echo 'Total (including delivery): ' . $total += $delivery/2;
+		break; 
+	case $total >= 100:
+		echo 'Total (free delivery): ' . $total;
+}
