be4806c9d1aa0bb435820523ae175f3fddf3219c
Haley Sparger <hsparg@users.noreply.github.com>
2018-06-30 20:48:24 -0500

    Create do_while_loop.php

---
+++ do_while_loop.php
@@ -0,0 +1,19 @@
+<?php
+//initialize counter
+$i = 100; 
+
+do {
+	//increment counter
+	$i++; 
+	echo $i . '<br>'; 
+	} while ($i < 10); 
+	
+  // The output is 101 because the program is executed but doesn't continue because the condition is false.
+  
+/* do..while Loop
+ * 
+ * do{
+ *		//repeat as long as the condition is true
+ *   } while (condition); 
+ *
+ */
