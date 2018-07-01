8e0f6e18659ddf0c7493b3b46904600c1d44f6d2
Haley Sparger <hsparg@users.noreply.github.com>
2018-06-30 22:02:14 -0500

    Create Alternative_Syntax_for_Loops.php

---
+++ Alternative_Syntax_for_Loops.php
@@ -0,0 +1,15 @@
+<?php
+//initialize counter
+// while Loop
+	$i = 0; 
+
+	while ($i < 10) : 
+		//increment counter
+		$i++; 
+		echo $i . '<br>';
+	endwhile;
+	
+//for Loop 
+	for($i = 0; $i < 10; $i++) :
+		echo $i . '<br>';
+	endfor; 
