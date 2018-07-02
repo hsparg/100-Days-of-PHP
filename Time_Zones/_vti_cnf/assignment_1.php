f987d860bbea51c02cec2411a60bd5f2d747146f
Haley Sparger <haley.sparger@ttu.edu>
2018-07-02 12:24:58 -0500

    Created a New Folder for Time Zone Projects

---
+++ Time_Zones/_vti_cnf/assignment_1.php
@@ -0,0 +1,16 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|LAPTOP-2BN3DATR\\haley
+vti_modifiedby:SR|LAPTOP-2BN3DATR\\haley
+vti_timelastmodified:TR|02 Jul 2018 17:12:54 -0000
+vti_timecreated:TR|02 Jul 2018 16:54:07 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_structuredtm:TR|02 Jul 2018 17:10:14 -0000
+vti_nexttolasttimemodified:TW|02 Jul 2018 17:12:01 -0000
+vti_cacheddtm:TX|02 Jul 2018 17:12:54 -0000
+vti_filesize:IR|354
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_charset:SR|utf-8
